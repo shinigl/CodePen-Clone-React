1534b954284e906e6cec98e1717f9965f3b34ba8
shinigl <kraniket754@gmail.com>
2025-01-26 17:56:29 +0530

    Implemented CodeMirror in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import './App.css'
 import Navbar from './components/NavBar';
+import CodeMirror from "@uiw/react-codemirror";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
+import { useCallback,useState } from "react";
+import Result from './components/Result';
 
 function App() {
   
+  const [html_edit, setHtml_Edit] = useState('');
+  const [css_edit, setCss_Edit] = useState('');
+  const [js_edit, setJs_Edit] = useState('');
+
+  //HTML handler
+  const onChangeHtml = useCallback((value)=>{
+    setHtml_Edit(value);
+  },[])
+  //CSS handler
+  const onChangeCss = useCallback((value)=>{
+    setCss_Edit(value);
+  },[])
+  //JS handler
+  const onChangeJavaScript = useCallback((value)=>{
+    setJs_Edit(value);
+  },[])
+
+  const srcCode = `
+  <html>
+  <body>${html_edit}</body>
+  <style>${css_edit}</style>
+  <script>${js_edit}</script>
+  </html>
+  `
 
   return (
     <>
@@ -14,21 +44,43 @@ function App() {
          {/* Html Editor */}
           <div className="bg-[#282c34] p-4 rounded-lg shadow">
             <h2 className="text-lg font-semibold mb-2 text-white">HTML</h2>
-            HTML
+            <CodeMirror
+              className="text-xl border-gray-700 border"
+              value={html_edit}
+              height="342px"
+              theme="dark"
+              extensions={[html(true)]}
+              onChange={onChangeHtml}
+            />
           </div>
 
           {/* Css Editor  */}
           <div className="bg-[#282c34] p-4 rounded-lg shadow">
             <h2 className="text-lg font-semibold mb-2 text-white">CSS</h2>
-            CSS
+            <CodeMirror
+              className="text-xl border-gray-700 border"
+              value={css_edit}
+              height="342px"
+              theme="dark"
+              extensions={[css(true)]}
+              onChange={onChangeCss}
+            />
           </div>
 
           {/* JavaScript Editor  */}
           <div className="bg-[#282c34] p-4 rounded-lg shadow">
             <h2 className="text-lg font-semibold mb-2 text-white">JavaScript</h2>
-            JAVASCRIPT
+            <CodeMirror
+              className="text-xl border-gray-700 border"
+              value={js_edit}
+              height="342px"
+              theme="dark"
+              extensions={[javascript(true)]}
+              onChange={onChangeJavaScript}
+            />
           </div>
         </div>
+        <Result srcCode={srcCode} />
       </div>
     </>
   )
